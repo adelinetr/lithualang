68781534e17c133f943aa6501f82f099d95b6a0d
Adelina <adelinatravina@gmail.com>
2023-08-08 19:00:47 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/HomePage/HomePage'
 import AboutPage from './components/AboutPage/AboutPage';
 import PricingPage from './components/PricingPage/PricingPage';
@@ -7,14 +7,14 @@ import ContactsPage from './components/ContactsPage/Contacts';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Router>
+      <Switch>
         <Route path="/" element={<Home />} />  
         <Route path="/about" element={<AboutPage />} />  
         <Route path="/pricing" element={<PricingPage />} />  
         <Route path="/contacts" element={<ContactsPage />} />  
-      </Routes>
-    </BrowserRouter>
+      </Switch>
+    </Router>
   );
 }
 
